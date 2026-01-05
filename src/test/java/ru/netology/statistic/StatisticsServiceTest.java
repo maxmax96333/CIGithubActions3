2343b75eb5155011f78fc3906485177c35bc357a
Max study netology <maxmax96333@gmail.com>
2026-01-05 18:00:01 +0300

    finished work

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -17,4 +17,59 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void maxFromSingleElement() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {7};
+        long expected = 7;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxFromAllNegativeAndZero() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = { -10, -5, -3, -20, 0 };
+        long expected = 0;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxAtFirstPosition() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {50, 20, 10, 5};
+        long expected = 50;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxAtLastPosition() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {1, 2, 3, 999};
+        long expected = 999;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void maxOccursSeveralTimes() {
+        StatisticsService service = new StatisticsService();
+        long[] incomes = {8, 1, 12, 12, 4, 12, 3};
+        long expected = 12;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
